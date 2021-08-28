b3a397122a69ed63ea03517947119b580d1b766a
Levi-Opunga <leviopunga@gmail.com>
2021-08-28 14:05:11 +0300

    replaced deleted files

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+import{HttpClientModule} from "@angular/common/http";
+
+import { AppComponent } from './app.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UserComponent } from './repositories/user/user.component';
+import { SearchFormComponent } from './repositories/user/search-form/search-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RepositoriesComponent,
+    UserComponent,
+    SearchFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/userdetails.ts
@@ -0,0 +1,13 @@
+export class User {
+  constructor(
+    public html_url: string,
+    public name:string,
+    public login:string,
+    public avatar_url:string,
+    public public_repos:number,
+    public created_at:Date,
+    public followers: number,
+    public following: number,
+
+  ){}
+}
