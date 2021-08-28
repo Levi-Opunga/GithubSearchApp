9729af94526bbdb1b53d9d616f2c4f920152a0fa
Levi-Opunga <leviopunga@gmail.com>
2021-08-28 13:51:38 +0300

    added repositories,users,searchform component and user details class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+import{HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UserComponent } from './repositories/user/user.component';
+import { SearchFormComponent } from './repositories/user/search-form/search-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoriesComponent,
+    UserComponent,
+    SearchFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
